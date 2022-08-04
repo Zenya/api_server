24d3d3bfced2074416af17a49700512100790373
Eugene <zenya.09@gmail.com>
2022-08-04 11:03:32 +0300

    Added seeds for Content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+content_example = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
+           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+           nisi ut aliquip ex ea commodo consequat.'
+
+summary_example = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, \
+                   eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+
+Content.create(title: 'Content 1', published_date: DateTime.new(2022,1,1,9), author: 'Author 1',
+               summary: summary_example, content: content_example, status: 'published')
+Content.create(title: 'Content 2', published_date: DateTime.new(2022,1,2,9), author: 'Author 1',
+               summary: summary_example, content: content_example, status: 'published')
+Content.create(title: 'Content 3', published_date: DateTime.new(2022,1,3,9), author: 'Author 2',
+               summary: summary_example, content: content_example, status: 'published')
+Content.create(title: 'Content 4', published_date: DateTime.new(2022,1,4,9), author: 'Author 2',
+               summary: summary_example, content: content_example, status: 'published')
+Content.create(title: 'Content 5', published_date: DateTime.new(2022,1,5,9), author: 'Author 1',
+               summary: summary_example, content: content_example, status: 'published')
+Content.create(title: 'Content 6', published_date: DateTime.new(2022,1,6,9), author: 'Author 3',
+               summary: summary_example, content: content_example, status: 'published')
+Content.create(title: 'Content 7', published_date: DateTime.new(2022,9,1,9), author: 'Author 1',
+               summary: summary_example, content: content_example, status: 'draft')
+Content.create(title: 'Content 8', published_date: DateTime.new(2022,9,2,9), author: 'Author 5',
+               summary: summary_example, content: content_example, status: 'draft')
+Content.create(title: 'Content 9', published_date: DateTime.new(2022,9,3,9), author: 'Author 4',
+               summary: summary_example, content: content_example, status: 'draft')
